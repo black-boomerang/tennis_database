557272277f7a3c2b5fdefc2e0cf21a3d3968212b
black-boomerang <alejo2000@yandex.ru>
2019-05-04 22:36:05 +0300

    Поправил форматирование

--- crud.sql
+++ crud.sql
@@ -1,14 +1,12 @@
 set search_path = tennis, public;
 
 -- Первый CRUD-запрос
-insert into sponsor (
-  sponsor_id,
-  sponsor_nm,
-  specialization_nm)
-values (
-  10,
-  'Xerox',
-  'Производство спортивного инвентаря');
+insert into sponsor (sponsor_id,
+                     sponsor_nm,
+                     specialization_nm)
+values (10,
+        'Xerox',
+        'Производство спортивного инвентаря');
 
 select *
 from sponsor
@@ -23,8 +21,7 @@ from sponsor
 where sponsor_nm = 'Xerox';
 
 -- Второй CRUD-запрос
-insert into match (
-  match_id, tournament_id, player1_id, player2_id, stage_nm, final_score, duration_val, match_dt)
+insert into match (match_id, tournament_id, player1_id, player2_id, stage_nm, final_score, duration_val, match_dt)
 values (12, 1, 1, 2, 'Полуфинал', '6-4, 7-6, 6-4', 164, '2019-01-24');
 
 select *
